88d6136a70b28eba1f4d083c5547ad6b6f49db73
zhangguowang <31911349+zhangguowang@users.noreply.github.com>
2018-12-02 09:59:26 +0800

    Add files via upload

---
+++ src/com/example/lazy_man_client/ReleaseTaskActivity.java
@@ -0,0 +1,61 @@
+package com.example.lazy_man_client;
+
+
+import com.example.mysocketdemo.R;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.DialogInterface.OnMultiChoiceClickListener;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
+
+
+public class ReleaseTaskActivity extends Activity {
+	
+
+    private ListView list;
+    String[] arr1 = new String[]{"任务一","任务二","任务三","任务四"};
+    @Override
+	protected void onCreate(Bundle savedInstanceState) {
+	       super.onCreate(savedInstanceState);
+	       setContentView(R.layout.activity_release_task);
+	       list = (ListView)findViewById(R.id.TaskListView);
+	       ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, R.layout.array_list_view, arr1);
+	       list.setAdapter(adapter1);
+	       list.setOnItemClickListener(new OnItemClickListener(){
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+				// arg2表示点击的是第几个列表
+				Intent intent =new Intent(ReleaseTaskActivity.this,ReDetailActivity.class);
+				//用Bundle携带数据
+			    Bundle bundle=new Bundle();
+			    //传递name参数为tinyphp
+			    bundle.putString("TaskId", "tinyphp");
+			    intent.putExtras(bundle);
+				startActivity(intent);
+			}	    	   
+	       });
+	      
+	    } 
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+    @Override
+    protected void onDestroy() {
+
+        super.onDestroy();
+    }
+
+}
